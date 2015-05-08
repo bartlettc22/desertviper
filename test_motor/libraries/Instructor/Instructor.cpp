befb514c774d61562253b508fb31dc013c6fc388
Chris Bartlett <christopher.bartlett@readytalk.com>
2015-05-08 16:55:07 -0600

    Added compass library and updated code to ready compass/hall sensors

---
+++ test_motor/libraries/Instructor/Instructor.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+#include "Instructor.h"
+
+void Instructor::Instructor()
+{
+ 
+}
+
+void Instructor::add(int command, int length, int length_unit)
+{
+ 
+}
---
+++ test_motor/libraries/Instructor/Instructor.h
@@ -0,0 +1,27 @@
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+ 
+#include <WProgram.h>
+ 
+#define INSTRUCT_COMMAND_DRIVE_FORWARD 0
+#define INSTRUCT_COMMAND_DRIVE_REVERSE 1
+#define INSTRUCT_COMMAND_DRIVE_STOP 2
+#define INSTRUCT_COMMAND_STEER_LEFT 3
+#define INSTRUCT_COMMAND_STEER_RIGHT 4
+#define INSTRUCT_COMMAND_STEER_STRAIGHT 5
+
+#define INSTRUCT_DURATION_UNIT_DIST 0
+#define INSTRUCT_DURATION_UNIT_TIME 1
+
+
+class Instructor {
+
+public:
+        Instructor();
+        ~Instructor();
+        void add();
+        //void off();
+        //void blink(int time);
+};
+ 
+#endif
